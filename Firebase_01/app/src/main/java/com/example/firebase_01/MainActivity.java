7283d62f78e5a623af4b31909ad9ea0d5d145942
Felix de Pablo <f.depablo.lobo@gmail.com>
2025-02-13 19:43:30 +0100

    ultimos cambios

--- Firebase_01/app/src/main/java/com/example/firebase_01/MainActivity.java
+++ Firebase_01/app/src/main/java/com/example/firebase_01/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.firebase_01;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +13,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,8 +30,24 @@ public class MainActivity extends AppCompatActivity {
         });
 
         final TextView tv = findViewById(R.id.mensaje1);
+        final EditText et = findViewById(R.id.editTextMensaje);
+        final Button bt = findViewById(R.id.buttonAdd);
 
-        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        final FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        bt.setOnClickListener(v -> {
+            String mensaje = et.getText().toString();
+            Mensaje m = new Mensaje();
+            m.setValor(mensaje);
+            db.collection("mensajes").add(m)
+                    .addOnSuccessListener(documentReference -> {
+                        Toast.makeText(MainActivity.this,
+                                "Todo bien",
+                                Toast.LENGTH_LONG).show();
+                    });
+        });
+
+
 
         db.collection("mensajes")
                 .document("2JdtOZ8Y0XhisWkK1HNe")
---
+++ Firebase_01/app/src/main/java/com/example/firebase_01/Mensaje.java
@@ -0,0 +1,13 @@
+package com.example.firebase_01;
+
+public class Mensaje {
+    private String valor;
+
+    public String getValor() {
+        return valor;
+    }
+
+    public void setValor(String valor) {
+        this.valor = valor;
+    }
+}
